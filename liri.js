d41eed6157ceb353aaae997de9d39a4d23337454
sisiishere <set38246@marymount.edu>
2019-07-12 15:37:30 -0400

    finalise

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 
 var axios = require('axios')
 
-var arguments = process.argv;
+var arguments = process.argv[2];
 
 require("dotenv").config();
 
@@ -16,19 +16,27 @@ var spotify = new Spotify(keys.spotify);
   //id: <your spotify client id></your>
  // secret: <your spotify client secret></your>
 //});
-
-if(arguments[2] == "spotify-this-song"){
-    spotify.search({ type: 'track', query: 'Mother Earth' }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data); 
-      });
+//process.argv
+if(arguments == "spotify-this-song"){
+    spotify.search({ type: 'track', query: 'Waiting Game' })
+      .then(function(response) {
+        //console.log(response) ;
+        console.log("****") ;
+        //console.log(response.tracks) ;
+        //console.log(response.tracks.items);
+        console.log(response.tracks.items[0].name);
+        console.log(response.tracks.items[0].artists[0].name);
+        console.log(response.tracks.items[0].album.name);
+        console.log(response.tracks.items[0].preview_url);
+      })
+      
+    .catch(function (err) {
+      console.log(err);
+    })
       
 }
 
-if(arguments[2] == "concert-this"){
+if(arguments == "concert-this"){
 
     var artist = "BANKS";
 
@@ -36,57 +44,98 @@ if(arguments[2] == "concert-this"){
 
 axios.get(queryURL).then(
     function(response) {  
-       console.log(response.data[0]); 
+       console.log(response.data[1].datetime); 
+       console.log(response.data[1].venue.name);
+       console.log(response.data[1].venue.city);
+       console.log(response.data[1].venue.country);
   console.log(response.Runtime);
 });
 }
-if(arguments[2] == "movie-this") {
+if(arguments == "movie-this") {
     var title = "jawbreaker";
 
 var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
 
 axios.get(queryURL).then(
     function(response) {
-      console.log(response);
+      console.log(response.data.Title);
+      console.log(response.data.Year);
+      console.log(response.data.imdbRating);
+      console.log(response.data.Ratings[1].Source);
+      console.log(response.data.Ratings[1].Value);
+      console.log(response.data.Country);
+      console.log(response.data.Language);
+      console.log(response.data.Plot);
+      console.log(response.data.Actors);
   console.log(response.Runtime);
 });
 }
-if(arguments[2] == "do-what-it-says") {
+if(arguments == "do-what-it-says") {
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
             return console.log(error);
           }
         
         var dataArr = data.split(",");
+        console.log(dataArr.length);
         var command = dataArr [0]
         var search = dataArr [1]
-        if (command == "spotify-this-song") {
-            spotify.search({ type: 'track', query: search}, function(err, data) {
-                if (err) {
-                  return console.log('Error occurred: ' + err);
-                }
-               
-              console.log(data); 
-              });
-        if (command == "movie-this") {
-            var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+        console.log(search);
+        if(command == "spotify-this-song"){
+         spotify.search({ type: 'track', query: search })
+           .then(function(response) {
+              //console.log(response) ;
+              //console.log("****") ;
+              //console.log(response.tracks) ;
+              //console.log(response.tracks.items);
+              console.log(response.tracks.items[0].name);
+              console.log(response.tracks.items[0].artists[0].name);
+              console.log(response.tracks.items[0].album.name);
+              console.log(response.tracks.items[0].preview_url);
+            })
+            
+          .catch(function (err) {
+            console.log(err);
+          })
+            
+      }
+      
+     if(command == "movie-this") {
+        var title = search;
+    
+    var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+    
+    axios.get(queryURL).then(
+        function(response) {
+          console.log(response.data.Title);
+          console.log(response.data.Year);
+          console.log(response.data.imdbRating);
+          console.log(response.data.Ratings[1].Source);
+          console.log(response.data.Ratings[1].Value);
+          console.log(response.data.Country);
+          console.log(response.data.Language);
+          console.log(response.data.Plot);
+          console.log(response.data.Actors);
+      console.log(response.Runtime);
+    });
+    }
 
-            axios.get(queryURL).then(
-                function(response) {
-                  console.log(response);
-              console.log(response.Runtime);
-            }); 
-        }
-        }
-        if (command == "concert-this") {
-            var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        
+       if(command == "concert-this"){
 
-            axios.get(queryURL).then(
-                function(response) {
-                  console.log(response.data[0]);
-              console.log(response.Runtime);
-            });   
-        }
+          var artist = search;
+      
+          var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+      
+      axios.get(queryURL).then(
+          function(response) {  
+             console.log(response.data[1].datetime); 
+             console.log(response.data[1].venue.name);
+             console.log(response.data[1].venue.city);
+             console.log(response.data[1].venue.country);
+        console.log(response.Runtime);
+      });
+      }
 }
  
 
